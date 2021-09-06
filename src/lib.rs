95e8a778fb4b324f5416a6a3f0bbd3d99145552b
Nicolas Chan <nicolas@nicolaschan.com>
2021-09-05 22:42:49 -0700

    Run cargo format

--- src/lib.rs
+++ src/lib.rs
@@ -1,16 +1,22 @@
-use std::{any::Any, sync::{Arc, Mutex}};
-use crossbeam::channel::{Receiver, RecvError, Sender, bounded, unbounded};
+use crossbeam::channel::{bounded, unbounded, Receiver, RecvError, Sender};
+use std::{
+    any::Any,
+    sync::{Arc, Mutex},
+};
 use thiserror::Error;
 
 #[cfg(test)]
 mod tests {
-    use std::{sync::{Arc, Mutex, mpsc::{Receiver, Sender, channel}}};
+    use std::sync::{
+        mpsc::{channel, Receiver, Sender},
+        Arc, Mutex,
+    };
 
     use crate::{ExecutionError, SendWrapperThread};
 
     #[test]
     fn test_send_pointer() {
-        let make_pointer = ||  {
+        let make_pointer = || {
             let my_box = Box::new(42);
             Box::into_raw(my_box)
         };
@@ -19,15 +25,19 @@ mod tests {
         let return_value_mutex: Arc<Mutex<Option<u128>>> = Arc::new(Mutex::new(None));
         let return_value_mutex_clone = return_value_mutex.clone();
         std::thread::spawn(move || {
-            let return_value = send_pointer.execute(|inner| {
-                let my_box = unsafe { Box::from_raw(inner) };
-                (Some(inner), *my_box + 1)
-            }).unwrap();
+            let return_value = send_pointer
+                .execute(|inner| {
+                    let my_box = unsafe { Box::from_raw(inner) };
+                    (Some(inner), *my_box + 1)
+                })
+                .unwrap();
 
-            let mut guard = return_value_mutex_clone.lock().unwrap(); 
+            let mut guard = return_value_mutex_clone.lock().unwrap();
             *guard = Some(return_value);
-        }).join().unwrap();
-        
+        })
+        .join()
+        .unwrap();
+
         let return_value = return_value_mutex.lock().unwrap().take().unwrap();
         assert_eq!(43, return_value);
     }
@@ -66,7 +76,7 @@ mod tests {
     fn test_could_not_send_error() {
         let mut wrapper = SendWrapperThread::new(|| ());
         wrapper.execute(|_inner| (None, ())).unwrap();
-        let result =  wrapper.execute(|_inner| (None, ()));
+        let result = wrapper.execute(|_inner| (None, ()));
         println!("{:?}", result);
         assert!(matches!(result, Err(ExecutionError::CouldNotSendError(..))));
         format!("{:?}", result); // should implement debug
@@ -104,23 +114,30 @@ pub struct SendWrapperThread<T: 'static> {
 
 impl<T: 'static> SendWrapperThread<T> {
     pub fn new(make_inner: impl (FnOnce() -> T) + Send + 'static) -> SendWrapperThread<T> {
-        let (outside_sender, inside_receiver): (Sender<SendCommand<T>>, Receiver<SendCommand<T>>) = bounded(1);
+        let (outside_sender, inside_receiver): (Sender<SendCommand<T>>, Receiver<SendCommand<T>>) =
+            bounded(1);
         std::thread::spawn(move || {
             let mut inner = make_inner();
             while let Ok(message) = inside_receiver.recv() {
-                let closure= message.closure;
+                let closure = message.closure;
                 let (inner_option, return_value) = closure(inner);
 
                 match inner_option {
                     Some(new_inner) => {
                         inner = new_inner;
-                        message.return_sender.send(return_value).expect("Failed to exfiltrate return values"); 
-                    },
+                        message
+                            .return_sender
+                            .send(return_value)
+                            .expect("Failed to exfiltrate return values");
+                    }
                     None => {
                         drop(inside_receiver);
-                        message.return_sender.send(return_value).expect("Failed to exfiltrate return values"); 
+                        message
+                            .return_sender
+                            .send(return_value)
+                            .expect("Failed to exfiltrate return values");
                         return;
-                    },
+                    }
                 };
             }
         });
@@ -129,7 +146,10 @@ impl<T: 'static> SendWrapperThread<T> {
         }
     }
 
-    pub fn execute<U: Any + Send + 'static>(&mut self, closure: impl (FnOnce(T) -> (Option<T>, U)) + Send + 'static) -> Result<U, ExecutionError> {
+    pub fn execute<U: Any + Send + 'static>(
+        &mut self,
+        closure: impl (FnOnce(T) -> (Option<T>, U)) + Send + 'static,
+    ) -> Result<U, ExecutionError> {
         let wrapped_closure: Box<RemoteExecutorClosure<T>> = Box::new(|inner| {
             let (new_inner, return_value) = closure(inner);
             let return_value_casted = Box::new(return_value);
@@ -140,11 +160,13 @@ impl<T: 'static> SendWrapperThread<T> {
             closure: wrapped_closure,
             return_sender: inside_sender,
         };
-        self.sender.send(send_message)
+        self.sender
+            .send(send_message)
             .map_err(|send_error| ExecutionError::CouldNotSendError(Box::new(send_error)))?;
-        let return_value = outside_receiver.recv()
+        let return_value = outside_receiver
+            .recv()
             .map_err(ExecutionError::NoResponseError)?;
-        let  return_value  = return_value.downcast::<U>().unwrap();
+        let return_value = return_value.downcast::<U>().unwrap();
         Ok(*return_value)
     }
 
@@ -159,9 +181,7 @@ impl<T: 'static> SendWrapperThread<T> {
 
 impl<T: 'static> Drop for SendWrapperThread<T> {
     fn drop(&mut self) {
-        let result = self.execute(|_inner| {
-            (None, ())
-        });
+        let result = self.execute(|_inner| (None, ()));
         if result.is_err() {
             // If we couldn't communicate, then it's probably already dropped
             // so do nothing.
